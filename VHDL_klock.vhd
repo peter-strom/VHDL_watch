87ca46e8bbe061c7c0239647501898fbf6093733
Peter Strömblad <peter@kungariket.se>
2022-06-08 15:11:55 +0200

    working clock and timer, some bugs

--- VHDL_klock.vhd
+++ VHDL_klock.vhd
@@ -19,7 +19,9 @@ end entity;
 architecture behaviour of VHDL_klock is
 	--w_ = watch
 	--t_ = timer
-	signal out_time_s, w_out_time_s, t_out_time_s : time_array_t;
+
+	signal out_time_s : time_array_t;
+	signal w_cnt_s, t_cnt_s : natural;
 	signal start_key_n_f, w_start_key_n_s, t_start_key_n_s : std_logic;
 	signal h_key_n_f, w_h_key_n_s, t_h_key_n_s : std_logic;
 	signal m_key_n_f, w_m_key_n_s, t_m_key_n_s : std_logic;
@@ -28,7 +30,12 @@ architecture behaviour of VHDL_klock is
 begin
 
 	watch : counter
-	generic map(CNT_1S_G => FREQUENCY_1HZ, CNT_LIM_G => 86399)
+	generic map(
+		CNT_1S_G => FREQUENCY_1HZ,
+		CNT_LIM_G => 86399,
+		PAUSED_G => '0',
+		COUNT_UP_G => '1'
+	)
 	port map(
 		clk => clk,
 		rst_n => rst_n,
@@ -36,11 +43,16 @@ begin
 		h_key_n => w_h_key_n_s,
 		m_key_n => w_m_key_n_s,
 		s_key_n => w_s_key_n_s,
-		out_time => w_out_time_s
+		cnt => w_cnt_s
 	);
 
 	timer : counter
-	generic map(CNT_1S_G => FREQUENCY_1HZ, CNT_LIM_G => 86399)
+	generic map(
+		CNT_1S_G => FREQUENCY_1HZ,
+		CNT_LIM_G => 86399,
+		PAUSED_G => '1',
+		COUNT_UP_G => '0'
+	)
 	port map(
 		clk => clk,
 		rst_n => rst_n,
@@ -48,7 +60,7 @@ begin
 		h_key_n => t_h_key_n_s,
 		m_key_n => t_m_key_n_s,
 		s_key_n => t_s_key_n_s,
-		out_time => t_out_time_s
+		cnt => t_cnt_s
 	);
 
 	h_H : seven_seg
@@ -83,57 +95,56 @@ begin
 	);
 
 	process (clk) is
-	variable start_key_flank_v : std_logic;
-	variable h_key_flank_v : std_logic;
-	variable m_key_flank_v : std_logic;
-	variable s_key_flank_v : std_logic;
+		variable start_key_flank_v : std_logic;
+		variable h_key_flank_v : std_logic;
+		variable m_key_flank_v : std_logic;
+		variable s_key_flank_v : std_logic;
 	begin
 		if rising_edge(clk) then
 			start_key_n_f <= start_key_n;
 			h_key_n_f <= h_key_n;
 			m_key_n_f <= m_key_n;
 			s_key_n_f <= s_key_n;
-			
-			if(start_key_n_f = '1' and start_key_n ='0') then
+
+			if (start_key_n_f = '0' and start_key_n = '1') then
 				start_key_flank_v := '1';
 			else
 				start_key_flank_v := '0';
 			end if;
 
-			if(h_key_n_f = '0' and h_key_n = '1') then
+			if (h_key_n_f = '0' and h_key_n = '1') then
 				h_key_flank_v := '1';
 			else
 				h_key_flank_v := '0';
 			end if;
 
-			if(m_key_n_f = '0' and m_key_n = '1') then
+			if (m_key_n_f = '0' and m_key_n = '1') then
 				m_key_flank_v := '1';
 			else
 				m_key_flank_v := '0';
 			end if;
 
-			if(s_key_n_f = '0' and s_key_n = '1') then
+			if (s_key_n_f = '0' and s_key_n = '1') then
 				s_key_flank_v := '1';
 			else
 				s_key_flank_v := '0';
 			end if;
-
-
-			if (func_sw = '1') then 			-- wach
-				out_time_s <= w_out_time_s;
-				w_start_key_n_s <= start_key_n_f;
-				w_h_key_n_s <= h_key_n_f; 
-				w_m_key_n_s <= m_key_n_f;
-				w_s_key_n_s <= s_key_n_f;
-				
-			else							-- timer
-				out_time_s <= t_out_time_s;
-				t_start_key_n_s <= start_key_n_f;
-				t_h_key_n_s <= h_key_n_f; 
-				t_m_key_n_s <= m_key_n_f;
-				t_s_key_n_s <= s_key_n_f;
+			if (func_sw = '1') then -- wach
+				out_time_s <= seconds_to_time_array(w_cnt_s);
+				w_start_key_n_s <= start_key_flank_v;
+				w_h_key_n_s <= h_key_flank_v;
+				w_m_key_n_s <= m_key_flank_v;
+				w_s_key_n_s <= s_key_flank_v;
+			else -- timer
+				out_time_s <= seconds_to_time_array(t_cnt_s);
+				t_start_key_n_s <= start_key_flank_v;
+				t_h_key_n_s <= h_key_flank_v;
+				t_m_key_n_s <= m_key_flank_v;
+				t_s_key_n_s <= s_key_flank_v;
 			end if;
+
 		end if;
+
 	end process;
 
 end architecture; -- arch
